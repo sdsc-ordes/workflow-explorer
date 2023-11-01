83d6e53d2f3fb1aa1f71f8157e97581eb2f93c97
supermaxiste <supermegaiperste@hotmail.com>
2023-11-01 09:25:04 +0100

    feat: added all current workflow information and updated app

--- app.R
+++ app.R
@@ -11,52 +11,48 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      helpText("Select features of interest to find a workflow manager for you"),
+      helpText("Select different constraints to find a workflow manager for you"),
       
-      selectInput("lang", "Programming language:",
-                 choices = c("Any",
-                             "Python", 
-                             "Language-agnostic")
+      selectInput("lang", "Is your workflow using only Python as language?",
+                 choices = c("Not relevant",
+                             "yes", 
+                             "no")
                  ),
       
-      selectInput("scale", "Workflow scale:",
-                  choices = c("Any",
-                              "micro", 
-                              "macro")
-                  ),
-      
-      selectInput("k8", "Kubernetes compatible:",
-                  choices = c("Any",
+      selectInput("k8", "Are you working or planning to work with Kubernetes?",
+                  choices = c("Not relevant",
                               "yes", 
                               "no")
       ),
       
-      selectInput("cloud", "Cloud compatible:",
-                  choices = c("Any",
+      selectInput("ml", "Are you using sklearn, tensorflow or other ML/AI libraries?",
+                  choices = c("Not relevant",
                               "yes", 
                               "no")
       ),
       
-      selectInput("ml", "Machine learning compatible:",
-                  choices = c("Any",
+      selectInput("containers", "Are you using or planning to use containers?",
+                  choices = c("Not relevant",
                               "yes", 
                               "no")
       ),
-      
-      selectInput("complexity", "Workflow complexity:",
-                  choices = c("Any",
-                              "low", 
-                              "high")
+
+      checkboxGroupInput("format", "Which format and/or language are you comfortable with?",
+                  choices = c("Python",
+                              "YAML", 
+                              "JSON",
+                              "R",
+                              "Groovy")
       ),
       
-      selectInput("data", "Amount of data:",
-                  choices = c("Any",
-                              "low", 
-                              "high")
+      selectInput("goal", "Is your end goal full automation (reproducibility+scheduling) or reproducibility only?",
+                  choices = c("Not relevant",
+                              "full automation", 
+                              "reproducibility only")
       ),
       
-      selectInput("containers", "Compatible with multi-container use:",
-                  choices = c("Any",
+      selectInput("combo", "Would you like to separate the pipeline and the automated scheduling?",
+                  choices = c("Not relevant",
                               "yes", 
                               "no")
       )
@@ -75,51 +71,55 @@ server <- function(input, output) {
     {
       
       workflowTable <- read.delim(here("workflowTable.tsv"))
-      if (input$lang != "Any") {
-        workflowTable <- workflowTable %>%
-          filter(language == input$lang)
+      if (input$lang != "Not relevant") {
+        if (input$lang == "yes") {
+          workflowTable <- workflowTable %>%
+            filter(language == "Python")
+        } else {
+          workflowTable <- workflowTable %>%
+            filter(language != "Python")
+        }
+        
       }
       
-      if (input$scale != "Any") {
-        workflowTable <- workflowTable %>%
-          filter(scale == input$scale)
-      }
-      
-      if (input$k8 != "Any") {
+      if (input$k8 != "Not relevant") {
         if (input$k8 == "yes") {
           workflowTable <- workflowTable %>%
-            filter(k8 == input$k8 | k8 == "yes-paid" | k8 == "yes-dev")
+            filter(k8 == input$k8 | k8 == "yes-but")
         } else {
         workflowTable <- workflowTable %>%
           filter(k8 == input$k8)
         }
       }
       
-      if (input$cloud != "Any") {
-        if (input$cloud == "yes") {
-          workflowTable <- workflowTable %>%
-            filter(cloud == input$k8 | cloud == "yes-paid")
-        } else {
+      if (input$containers != "Not relevant") {
         workflowTable <- workflowTable %>%
-          filter(cloud == input$cloud)
+          filter(containers == input$containers)
+      }
+      
+      if (input$ml != "Not relevant") {
+        workflowTable <- workflowTable %>%
+          filter(ML-compatible == input$ml)
+      }
+      
+      if (!is.null(input$format)) {
+        workflowTable <- workflowTable %>%
+          filter(format %in% input$format)
+      }
+      
+      if (input$goal != "Not relevant") {
+        if (input$goal == "full automation") {
+          workflowTable <- workflowTable %>%
+            filter(goal == "auto")
+        } else {
+          workflowTable <- workflowTable %>%
+            filter(goal == "pipeline")
         }
       }
       
-      if (input$complexity != "Any") {
+      if (input$combo != "Not relevant") {
         workflowTable <- workflowTable %>%
-          filter(wf.complexity == input$complexity |
-                   wf.complexity == "low_and_high")
-      }
-      
-      if (input$data != "Any") {
-        workflowTable <- workflowTable %>%
-          filter(data.amount == input$data.amount |
-                   data.amount == "low_and_high")
-      }
-      
-      if (input$containers != "Any") {
-        workflowTable <- workflowTable %>%
-          filter(containers == input$containers)
+          filter(combo == input$combo)
       }
       
       workflowTable
